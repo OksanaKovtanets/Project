bb246a32fb4e08d89cf54ed4b0b253365abb6c45
Oksana Kovtanets <oksanakovtanets@gmail.com>
2021-01-14 21:59:15 +0100

    Step 3

--- Training zone/js/script.js
+++ Training zone/js/script.js
@@ -1,8 +1,16 @@
 "use strict";
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
-let personalMovieDB = {
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
   genres: [],
   privat: false
 };
+const lastWatchedFilm1 = prompt("Укажите один из последних просмотренных фильмов",""),
+      raiting1 = prompt("На сколько оцените его?"),
+      lastWatchedFilm2 = prompt("Укажите один из последних просмотренных фильмов",""),
+      raiting2 = prompt("На сколько оцените его?");
+personalMovieDB.movies[lastWatchedFilm1] = raiting1;  
+personalMovieDB.movies[lastWatchedFilm2] = raiting2;
+
+console.log(personalMovieDB);
