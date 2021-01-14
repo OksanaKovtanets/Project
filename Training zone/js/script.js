84fa86bc6b2d7679aba8cf1804d33eb9d48b13b9
Oksana Kovtanets <oksanakovtanets@gmail.com>
2021-01-14 21:37:27 +0100

    Step2

--- Training zone/js/script.js
+++ Training zone/js/script.js
@@ -1,2 +1,8 @@
 "use strict";
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  genres: [],
+  privat: false
+};
